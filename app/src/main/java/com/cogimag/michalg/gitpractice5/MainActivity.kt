e6e518538ec898ce29f4432d441a9f769b72891e
Michal G <33378775+Galajda@users.noreply.github.com>
2019-06-02 07:21:48 -0500

    added log tag constant

--- app/src/main/java/com/cogimag/michalg/gitpractice5/MainActivity.kt
+++ app/src/main/java/com/cogimag/michalg/gitpractice5/MainActivity.kt
@@ -5,12 +5,12 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-
+    private val logTag = "git practice 5"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
     fun sayHello() {
-        Log.i("app tag", "hello")
+        Log.i(logTag, "hello")
     }
 }
